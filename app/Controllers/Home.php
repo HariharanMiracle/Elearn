9d5f0a2c64c63dbba757911b5b97ee37785ba19c
IT17044400_Hariharan.V <it17044400@my.sliit.lk>
2020-12-21 23:15:45 +0530

    Home page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,6 +1,13 @@
 <?php namespace App\Controllers;
 
 use App\Models\SettingModel;
+use App\Models\TagsModel;
+use App\Models\VideosModel;
+use App\Models\EventsModel;
+use App\Models\NoticeModel;
+use App\Models\NewsModel;
+use App\Models\CourseModel;
+use App\Models\BooksModel;
 
 class Home extends BaseController
 {
@@ -10,6 +17,15 @@ class Home extends BaseController
 		$settingModel = new SettingModel();
 	    $data['setting'] = $settingModel->orderBy('id', 'ASC')->findAll();
 		$data['nav'] = "home";
+
+		$newsModel = new NewsModel();
+		$data['news'] = $newsModel->orderBy('postedOn', 'DESC')->findAll();
+		
+		$eventsModel = new EventsModel();
+		$data['events'] = $eventsModel->orderBy('postedOn', 'DESC')->findAll();
+		
+		$noticeModel = new NoticeModel();
+	    $data['notice'] = $noticeModel->orderBy('postedOn', 'DESC')->findAll();
 
 		echo view('templates/header', $data);
 		echo view('home');
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,16 +1,16 @@
 <br/>
 <br/>
 <br/>
-<main class="container">
+<main class="container-fluid">
     <div>
         <?php
             if($_SESSION['isLoggedIn'] == 1){
-                echo '<h1>Welcome '.$_SESSION['user']['fname'].' '.$_SESSION['user']['lname'].'</h1>';
+                echo '<div class="text-center"><h1>Welcome '.$_SESSION['user']['fname'].' '.$_SESSION['user']['lname'].'</h1></div>';
             }
             else{
                 foreach($setting as $settingObj){
                     if($settingObj['tkey'] == 'company'){
-                        echo '<h1>Welcome to '.$settingObj['value'].'</h1>';
+                        echo '<div class="text-center"><h1>Welcome to '.$settingObj['value'].'</h1></div>';
                     }
                 }
             }
@@ -18,6 +18,101 @@
         <hr/>
     </div>
     
+    <div class="row" style="padding: 25px;">
+        <div class="col-md-4 text-center">
+            <h4>News / Updates</h4>
+        </div>
+
+        <div class="col-md-4 text-center">
+            <h4>Events</h4>
+        </div>
+
+        <div class="col-md-4 text-center">
+            <h4>Notice</h4>
+        </div>
+    </div>
+
+    <div class="row" style="padding: 10px;">
+        <div class="col-md-4 pre-scrollable">
+            <?php
+                $count = 1;
+                foreach ($news as $newsObj) {
+                    ?>
+                        <div class="card">
+                            <div class="text-center bg-info"><h3><?php echo $newsObj['title']; ?></h3></div>
+                            <div class="text-center"><?php echo '<img src="'.base_url().'/uploads/images/news/'.$newsObj['image'].'" class="card-img-top"/>'; ?></div>
+                            <div class="card-body">
+                                <?php echo $newsObj['description']; ?>
+                                <?php echo '<a class="text-info" href="'.$newsObj['link'].'">'.$newsObj['link'].'</a>'; ?>
+                            </div>
+                            <div class="card-footer bg-info text-center" style="padding: 10px;">
+                            <small class="text-muted"><?php echo '<b>News Date:</b> '.$newsObj['newsDate']; ?></small> <b>/</b>
+                            <small class="text-muted"><?php echo '<b>News Time:</b> '.$newsObj['newsTime']; ?></small> <b>/</b>
+                            <small class="text-muted"><?php echo '<b>Posted On:</b> '.$newsObj['postedOn']; ?></small> 
+                            </div>
+                        </div>
+                        <br/>
+                        <hr/>
+                        <br/>
+                    <?php
+                    $count++;
+                }
+            ?>
+        </div>
+
+        <div class="col-md-4 pre-scrollable">
+            <?php
+                $count = 1;
+                foreach ($events as $eventsObj) {
+                    ?>
+                        <div class="card">
+                            <div class="text-center bg-info"><h3><?php echo $eventsObj['title']; ?></h3></div>
+                            <div class="text-center"><?php echo '<img src="'.base_url().'/uploads/images/events/'.$eventsObj['image'].'" class="card-img-top"/>'; ?></div>
+                            <div class="card-body">
+                                <h5><b>Zoom Link:</b> <?php echo '<a class="text-info" href="'.$eventsObj['link'].'">'.$eventsObj['link'].'</a>'; ?></h5>
+                                <h5><b>Meeting Id:</b> <?php echo $eventsObj['meetingId']; ?></h5>
+                                <h5><b>Passcode:</b> <?php echo $eventsObj['passcode']; ?></h5>
+                                <h5><b>Time Zone:</b> <?php echo $eventsObj['timeZone']; ?></h5>
+                            </div>
+                            <div class="card-footer bg-info text-center" style="padding: 10px;">
+                            <small class="text-muted"><?php echo '<b>Event Date:</b> '.$eventsObj['eventDate']; ?></small> <b>/</b>
+                            <small class="text-muted"><?php echo '<b>Event Time:</b> '.$eventsObj['eventTime']; ?></small> <b>/</b>
+                            <small class="text-muted"><?php echo '<b>Posted On:</b> '.$eventsObj['postedOn']; ?></small>
+                            </div>
+                        </div>
+                        <br/>
+                        <hr/>
+                        <br/>
+                    <?php
+                    $count++;
+                }
+            ?>
+        </div>
+
+        <div class="col-md-4 pre-scrollable">
+            <?php
+                $count = 1;
+                foreach ($notice as $noticeObj) {
+                    ?>
+                        <div class="card">
+                            <div class="text-center bg-info"><h3><?php echo $noticeObj['title']; ?></h3></div>
+                            <div class="text-center"><?php echo '<img src="'.base_url().'/uploads/images/notice/'.$noticeObj['image'].'" class="card-img-top"/>'; ?></div>
+                            <div class="card-body">
+                                <?php echo $noticeObj['description']; ?>
+                            </div>
+                            <div class="card-footer bg-info text-center" style="padding: 10px;">
+                                <small class="text-muted"><?php echo '<b>Posted On:</b> '.$noticeObj['postedOn']; ?></small>
+                            </div>
+                        </div>
+                        <br/>
+                        <hr/>
+                        <br/>
+                    <?php
+                    $count++;
+                }
+            ?>
+        </div>
+    </div>
 </main>
 <br/>
 <br/>
